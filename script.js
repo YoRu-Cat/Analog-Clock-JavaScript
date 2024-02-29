6898f0f9579f4844825449c5d3a2191a91dee3db
YoRu Tech <tech.niko.coding@gmail.com>
2024-02-29 14:18:58 +0500

    Version 1

---
+++ script.js
@@ -0,0 +1,17 @@
+//create number hours
+
+//create bar seconds
+
+/* -------------------------------------------------------
+60 seconds = 360deg so 1 sec = 360 / 60 = 6deg
+60 minutes = 360deg so 1 min = 360 / 60 = 6deg
+12 hours = 360deg so 1 hrs = 360 / 12 = 30deg
+
+1 hrs = 30deg that means 60 min = 30deg so 1 min = 30 / 60 = 0.5 or 1/2 deg
+
+so formula for hours is (hours * 30 + minutes / 2)
+------------------------------------------------------- */
+
+//call getCurrentTime function on page load
+
+//call getCurrentTime to set clock hands every second
